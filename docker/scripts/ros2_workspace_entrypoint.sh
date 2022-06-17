2566012f3d8af2a23ea6c0032f43060f74210ea9
Andrea Sorbini <asorbini@rti.com>
2022-06-17 13:50:51 -0700

    Support full binary installation

--- docker/scripts/ros2_workspace_entrypoint.sh
+++ docker/scripts/ros2_workspace_entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /opt/rti.com/rti_connext_dds.arch ]; then
+  export CONNEXTDDS_ARCH=$(cat /opt/rti.com/rti_connext_dds.arch)
+fi
+
 /fix_gcc.sh
 
 if [ "$@" = "__default__" ]; then
